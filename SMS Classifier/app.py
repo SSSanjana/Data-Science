d3192c99166210de4722bfbc3ad3285b603481f7
SSSanjana <84617410+SSSanjana@users.noreply.github.com>
2024-12-18 10:54:22 +0530

    Add files via upload

---
+++ SMS Classifier/app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pickle
+from sklearn.feature_extraction.text import TfidfTransformer
+
+#Loading the saved vectorizer and Naive Bayes model 
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import string
+
+nltk.download('stopwords')
+
+ps = PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    text = [word for word in text if word.isalnum()]
+
+    text = [word for word in text if word not in stopwords.words('english') and word not in string.punctuation]
+
+    text = [ps.stem(word) for word in text]
+
+    return " ".join(text)
+
+st.title('Email/SMS Spam Classifier')
+input_sms = st.text_area("Enter message")
+
+if st.button('Predict'):
+    transformed_sms = transform_text(input_sms)
+
+    vector_input = tfidf.transform([transformed_sms])
+
+    result = model.predict(vector_input)[0]
+
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
